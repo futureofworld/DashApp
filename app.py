e79f91c185b298833e3dfee25ccec66e4a0a62a1
Rahul Dhavale <rahul@gmail.com>
2023-05-06 18:22:48 +0530

    created app.py

---
+++ app.py
@@ -0,0 +1,50 @@
+import pandas as pd
+from dash import Dash, dcc, html
+
+data = (
+    pd.read_csv("https://raw.githubusercontent.com/chainhaus/pythoncourse/master/avocado.csv")
+    .query(" type == 'conventional' and region == 'Albany' ")
+    .assign(Date=lambda data: pd.to_datetime(data["Date"], format="%Y-%m-%d"))
+    .sort_values(by="Date")
+)
+
+app = Dash(__name__)
+
+app.layout = html.Div(
+    children=[
+        html.H1(children="Avocado Analytics"),
+        html.P(
+            children=(
+                "Analyze the behavior of avocado prices and the number"
+                " of avocados sold in the US between 2015 and 2018"
+            ),
+        ),
+        dcc.Graph(
+            figure={
+                "data": [
+                    {
+                        "x": data["Date"],
+                        "y": data["AveragePrice"],
+                        "type": "lines",
+                    },
+                ],
+                "layout" : {"title": "Average Price of Avocado"},
+            },
+        ),
+        dcc.Graph(
+            figure={
+                "data": [
+                    {
+                        "x": data["Date"],
+                        "y": data["Total Volume"],
+                        "type": "lines"
+                    },
+                ],
+                "layout": {"title": "Avocados Sold"},
+            },
+        ),
+    ]
+)
+
+if __name__ == "__main__":
+    app.run_server(host="0.0.0.0", port=8080, debug=True)
